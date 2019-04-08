7ec40a606f023247aed24ccf2a353b22f50f1082
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:18:10 +0000

    Mon Apr 08 13:18:10 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -4,7 +4,8 @@ class FizzBuzz
   def fizz_buzz(number)
     str_numb = number.to_s
     str_ary = str_numb.split('')
-    print str_ary.uniq
+    print str_ary.uniq.length
+
 
     if  str_ary.uniq.length == 1 && number % 15 == 0 || str_ary.each.include?('3') && number % 5 == 0 || str_ary.each.include?('5') && number % 3 == 0 || str_ary.each.include?('5') && str_ary.each.include?('3')
       return 'fizz buzz deluxe'
@@ -27,6 +28,7 @@ class FizzBuzz
   end
 
 end
+
 
 
 
