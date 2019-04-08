f68aefb0fb613c24494212e0ab1372df85d3407e
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 11:24:27 +0000

    Mon Apr 08 11:24:27 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -5,7 +5,7 @@ class FizzBuzz
     str_numb = number.to_s
     str_ary = str_numb.split('')
     print str_ary
-    if str_ary.each.include('3')
+    if str_ary.each.include?('3')
       puts 'cool'
     else
       puts 'not cool'
@@ -17,11 +17,11 @@ class FizzBuzz
       return 'buzz'
     elsif number % 3 == 0
       return 'fizz'
-    elsif str_numb.include?('5' && '3')
+    elsif str_ary.each.include?('5') && str_ary.each.include?('3')
       return  'fizz buzz'
-    elsif str_numb.include('5')
+    elsif str_ary.each.include?('5')
       return 'buzz'
-    elsif str_numb.include('3')
+    elsif str_ary.each.include?('3')
       return 'fizz'
     else
       return number
@@ -30,6 +30,7 @@ class FizzBuzz
   end
 
 end
+
 
 
 
