6a9e4c720d53ec19a7a87aa097567128cb1b53ae
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 12:15:10 +0000

    Mon Apr 08 12:15:10 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -5,21 +5,11 @@ class FizzBuzz
     str_numb = number.to_s
     str_ary = str_numb.split('')
 
-    if number % 15 == 0
+    if number % 15 == 0 || str_ary.each.include?('3') && number % 5 == 0 || str_ary.each.include?('5') && number % 3 == 0 || str_ary.each.include?('5') && str_ary.each.include?('3')
       return 'fizz buzz'
-    elsif str_ary.each.include?('3') && number % 5 == 0
-      return 'fizz buzz'
-    elsif number % 5 == 0
+    elsif number % 5 == 0 || str_ary.each.include?('5')
       return 'buzz'
-    elsif str_ary.each.include?('5') && number % 3 == 0
-      return 'fizz buzz'
-    elsif number % 3 == 0
-      return 'fizz'
-    elsif str_ary.each.include?('5') && str_ary.each.include?('3')
-      return  'fizz buzz'
-    elsif str_ary.each.include?('5')
-      return 'buzz'
-    elsif str_ary.each.include?('3')
+    elsif number % 3 == 0 || str_ary.each.include?('3')
       return 'fizz'
     else
       return number
@@ -28,4 +18,5 @@ class FizzBuzz
   end
 
 end
+
 
