5d2e707b0e4ac356d301d740b4959258e09fa2dc
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 10:59:43 +0000

    Mon Apr 08 10:59:43 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,7 +2,14 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-    raise 'Not implemented'
+    if number % 5 == 0
+      return 'buzz'
+    elsif number % 3 == 0
+      return 'fizz'
+    else
+     return number
+   end
   end
 
 end
+
--- spec/fiz_r1_spec.rb
+++ spec/fiz_r1_spec.rb
@@ -1,3 +1,12 @@
 require './lib/solutions/FIZ/fizz_buzz.rb'
 
-describe "FIZZBUZZ, if a number is a multiple of three then it returns 'fizz' "
+describe "FIZZBUZZ, if a number is a multiple of three then it returns 'fizz'" do
+  it 'returns fizz when a multiple of three' do
+    expect(FizzBuzz.new.fizz_buzz(9)).to eq('fizz')
+  end
+
+  it 'returns buzz when a multiple of five' do
+    expect(FizzBuzz.new.fizz_buzz(10)).to eq('buzz')
+  end
+end
+
