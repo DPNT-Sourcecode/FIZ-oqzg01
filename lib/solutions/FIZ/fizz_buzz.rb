4aadf4b3437715d015aa8b7a04b3d387d14090c3
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 14:47:14 +0000

    Mon Apr 08 14:47:14 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -17,23 +17,23 @@ class FizzBuzz
       result.push 'fizz'
     elsif number % 3 == 0 && str_ary.each.include?('3') || number % 5 == 0 && str_ary.each.include?('5')
       result.push 'deluxe'
-    elsif number.even? == false && str_ary.length > 1 && str_ary.uniq.length == 1
+    elsif number.even? == false
       result.push 'fake deluxe'
     else
       return number
     end
 
 
-    if number % 3 == 0 && str_ary.each.include?('3') && number.even? == true && str_ary.length > 1
+    if number % 3 == 0 && str_ary.each.include?('3') && number.even? == true
       result.push 'deluxe'
     end
-    if number % 5 == 0 && str_ary.each.include?('5') && number.even? == true && str_ary.length > 1
+    if number % 5 == 0 && str_ary.each.include?('5') && number.even? == true
       result.push 'deluxe'
     end
-    if number % 3 == 0 && str_ary.each.include?('3') && number.even? == false && str_ary.length > 1 && str_ary.uniq.length == 1
+    if number % 3 == 0 && str_ary.each.include?('3') && number.even? == false
       result.push 'fake deluxe'
     end
-    if number % 5 == 0 && str_ary.each.include?('5') && number.even? == false && str_ary.length > 1 && str_ary.uniq.length == 1
+    if number % 5 == 0 && str_ary.each.include?('5') && number.even? == false
       result.push 'fake deluxe'
     end
 
@@ -44,5 +44,6 @@ class FizzBuzz
   end
 
 end
+
 
 
