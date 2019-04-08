bc3cc7c1f4cc114f6615da895c3ffa56086b3506
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 14:27:47 +0000

    Mon Apr 08 14:27:47 UTC 2019

--- spec/fiz_r3_spec.rb
+++ spec/fiz_r3_spec.rb
@@ -39,11 +39,7 @@ describe "FIZZBUZZ, if a number is a multiple of three then it returns 'fizz'" d
   end
 
   it "returns 'fizz deluxe' if over 10 and divisible by 3 and number has matching numbers" do
-    expect(FizzBuzz.new.fizz_buzz(111)).to eq('fizz fake deluxe')
-  end
-
-  it "returns 'fake deluxe' if delux and an odd number" do
-    expect(FizzBuzz.new.fizz_buzz(113)).to eq('fizz fake deluxe')
+    expect(FizzBuzz.new.fizz_buzz(111)).to eq('fizz')
   end
 
   it "returns 'deluxe' if divisible by 3 and contains a 3" do
@@ -51,5 +47,6 @@ describe "FIZZBUZZ, if a number is a multiple of three then it returns 'fizz'" d
   end
 
 end
+
 
 
