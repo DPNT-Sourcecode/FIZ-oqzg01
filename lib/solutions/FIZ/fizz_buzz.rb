e69fb931dbf6afc36f4f91265040a48369143256
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 11:33:08 +0000

    Mon Apr 08 11:33:08 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -13,18 +13,18 @@ class FizzBuzz
 
     if number % 15 == 0
       return 'fizz buzz'
+    elsif str_ary.each.include?('3') && number % 5 == 0
+      return 'fizz buzz'
     elsif number % 5 == 0
       return 'buzz'
+    elsif str_ary.each.include?('5') && number % 3 == 0
+      return 'fizz buzz'
     elsif number % 3 == 0
       return 'fizz'
     elsif str_ary.each.include?('5') && str_ary.each.include?('3')
       return  'fizz buzz'
-    elsif str_ary.each.include?('5') && number % 3 == 0
-      return 'fizz buzz'
     elsif str_ary.each.include?('5')
       return 'buzz'
-    elsif str_ary.each.include?('3') && number % 5 == 0
-      return 'fizz buzz'
     elsif str_ary.each.include?('3')
       return 'fizz'
     else
@@ -34,4 +34,5 @@ class FizzBuzz
   end
 
 end
+
 
