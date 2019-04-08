1ee290f3d83270de3061abaecb2f8100f46aa50e
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 10:53:02 +0000

    Mon Apr 08 10:53:02 UTC 2019

--- lib/solutions/HLO/hello.rb
+++ lib/solutions/HLO/hello.rb
@@ -3,10 +3,11 @@ class Hello
 
   def hello(friend_name)
     if friend_name.is_a? String
-      return "Hello, World!"
+      return "Hello, #{friend_name}!"
     else
       raise 'Not implemented'
     end
   end
 
 end
+
