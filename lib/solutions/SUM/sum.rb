36c572e2b80721e5ecf02a0b9877dce49c026596
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 10:15:26 +0000

    Mon Apr 08 10:15:26 UTC 2019

--- lib/solutions/SUM/sum.rb
+++ lib/solutions/SUM/sum.rb
@@ -2,7 +2,13 @@
 class Sum
 
   def sum(x, y)
-    raise 'Not implemented'
+    answer = x + y
+    if answer.is_a? Integer == true
+      return answer
+    else
+      raise 'Not implemented'
+    end
   end
 
 end
+
