8fc6eeea8271b9a7c87b9bfdb2e762ae228a0bec
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 12:57:10 +0000

    Mon Apr 08 12:57:10 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -4,21 +4,13 @@ class FizzBuzz
   def fizz_buzz(number)
     str_numb = number.to_s
     str_ary = str_numb.split('')
+    print str_ary.all
 
     if number % 15 == 0 || str_ary.each.include?('3') && number % 5 == 0 || str_ary.each.include?('5') && number % 3 == 0 || str_ary.each.include?('5') && str_ary.each.include?('3')
-      if str_ary.all? == 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9
-        return 'fizz buzz deluxe'
-      end
       return 'fizz buzz'
     elsif number % 5 == 0 || str_ary.each.include?('5')
-      if str_ary.all? == 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9
-        return 'buzz deluxe'
-      end
       return 'buzz'
     elsif number % 3 == 0 || str_ary.each.include?('3')
-      if str_ary.all? == 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9
-        return 'fizz deluxe'
-      end
       return 'fizz'
     elsif str_ary.all? == 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9
       return 'deluxe'
@@ -29,4 +21,5 @@ class FizzBuzz
   end
 
 end
+
 
