2db92cb9036bb935a55db75ff09223be8a563c6f
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 10:53:27 +0000

    Mon Apr 08 10:53:27 UTC 2019

--- spec/hlo_r1_spec.rb
+++ spec/hlo_r1_spec.rb
@@ -2,10 +2,8 @@ require './lib/solutions/HLO/hello.rb'
 
 describe "returns 'hello world'" do
   it 'should take a string and return hello + the string passed' do
-    expect(Hello.new.hello("World!")).to eq("Hello, World!")
+    expect(Hello.new.hello("World")).to eq("Hello, World!")
   end
 
-  it "should always return 'Hello, World!'" do
-    expect(Hello.new.hello("BOB")).to eq("Hello, World!")
-  end
 end
+
