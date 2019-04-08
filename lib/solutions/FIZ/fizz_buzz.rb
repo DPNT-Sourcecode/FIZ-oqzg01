012d5a4fea26d5ba8c0633aba81a223d75cc5ee0
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:15:10 +0000

    Mon Apr 08 13:15:10 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -4,12 +4,13 @@ class FizzBuzz
   def fizz_buzz(number)
     str_numb = number.to_s
     str_ary = str_numb.split('')
+    print str_ary.uniq
 
-    if  str_ary.uniq.length != str_ary.length && number % 15 == 0 || str_ary.each.include?('3') && number % 5 == 0 || str_ary.each.include?('5') && number % 3 == 0 || str_ary.each.include?('5') && str_ary.each.include?('3')
+    if  str_ary.uniq.length == 1 && number % 15 == 0 || str_ary.each.include?('3') && number % 5 == 0 || str_ary.each.include?('5') && number % 3 == 0 || str_ary.each.include?('5') && str_ary.each.include?('3')
       return 'fizz buzz deluxe'
-    elsif str_ary.uniq.length != str_ary.length && number % 5 == 0 || str_ary.each.include?('5')
+    elsif str_ary.uniq.length == 1 && number % 5 == 0 || str_ary.each.include?('5')
       return 'buzz deluxe'
-    elsif str_ary.uniq.length != str_ary.length && number % 3 == 0 || str_ary.each.include?('3')
+    elsif str_ary.uniq.length == 1 && number % 3 == 0 || str_ary.each.include?('3')
       return 'fizz deluxe'
     elsif number % 15 == 0 || str_ary.each.include?('3') && number % 5 == 0 || str_ary.each.include?('5') && number % 3 == 0 || str_ary.each.include?('5') && str_ary.each.include?('3')
       return 'fizz buzz'
@@ -26,6 +27,7 @@ class FizzBuzz
   end
 
 end
+
 
 
 
