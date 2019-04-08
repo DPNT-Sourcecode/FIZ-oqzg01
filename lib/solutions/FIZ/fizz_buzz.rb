604f862d8467fda56a642c85c065719090e10953
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 12:54:10 +0000

    Mon Apr 08 12:54:10 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -6,10 +6,19 @@ class FizzBuzz
     str_ary = str_numb.split('')
 
     if number % 15 == 0 || str_ary.each.include?('3') && number % 5 == 0 || str_ary.each.include?('5') && number % 3 == 0 || str_ary.each.include?('5') && str_ary.each.include?('3')
+      if str_ary.all? == 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9
+        return 'fizz buzz deluxe'
+      end
       return 'fizz buzz'
     elsif number % 5 == 0 || str_ary.each.include?('5')
+      if str_ary.all? == 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9
+        return 'buzz deluxe'
+      end
       return 'buzz'
     elsif number % 3 == 0 || str_ary.each.include?('3')
+      if str_ary.all? == 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9
+        return 'fizz deluxe'
+      end
       return 'fizz'
     elsif str_ary.all? == 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9
       return 'deluxe'
@@ -20,3 +29,4 @@ class FizzBuzz
   end
 
 end
+
